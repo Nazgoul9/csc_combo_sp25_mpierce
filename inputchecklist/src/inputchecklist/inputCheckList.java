daa2f27fc5dd85af087f56e31c5d380266efbd69
wannabewarlock <Madmorgyden@gmail.com>
2025-02-06 09:52:44 -0700

    check sheet for input

--- inputchecklist/src/inputchecklist/inputCheckList.java
+++ inputchecklist/src/inputchecklist/inputCheckList.java
@@ -13,21 +13,29 @@ public class inputCheckList {
 		float realnumb;
 		double biggerrealnum;
 		char letter;
-//		System.out.println("the word goes here");
-//		word = input.next();
-//		System.out.println("words go here " + word);
-//		System.out.println("the int goes here");
-//		intnum = input.nextInt();
-//		System.out.println("yay, an int "+intnum);
-//		System.out.println("the floating decimal point goes here");
-//		realnumb = input.nextFloat();
-//		System.out.println("yay a floating decimal point "+realnumb);
+		System.out.println("the sentence goes here");
+		word = input.nextLine();
+		System.out.println("words go here " + word);
+		System.out.println("the int goes here");
+		intnum = input.nextInt();
+		System.out.println("yay, an int "+intnum);
+		System.out.println("the floating decimal point goes here");
+		realnumb = input.nextFloat();
+		realnumb = realnumb + realnumb;
+		System.out.printf("%s,%.9f","yay a floating decimal point ",realnumb);
 		System.out.println("the large floating decimal point goes here");
 		biggerrealnum=input.nextDouble();
-		System.out.println("yay a large floating decimal point"+biggerrealnum);
+		biggerrealnum = biggerrealnum + biggerrealnum;
+		System.out.printf("%s %.9f","yay a large floating decimal point",biggerrealnum);
 		System.out.println("the leter goes here");
 		letter = input.next().charAt(0);
 		System.out.println("yay a letter " + letter);
+//		
+//		String str = "hello world";
+//		str = str.toUpperCase();
+//
+//		System.out.println(str);
+//		outputs:HELLO WORLD
 		
 	}
 
